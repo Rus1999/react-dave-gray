db862dd99c25f27ec2383907d8ca89920b3acc3c
Rus@home <punyarit07@gmail.com>
2024-03-24 19:17:47 +0700

    make sub component

--- src/ItemList.js
+++ src/ItemList.js
@@ -1,10 +1,9 @@
-import { FaTrashAlt } from 'react-icons/fa';
 import LineItem from './LineItem';
 
 const ItemList = ({ items, handleCheck, handleDelete }) => {
   return (
     <ul>
-      {items.map(item => (
+      {items.map((item) => (
         <LineItem
           key={item.id}
           item={item}
