91cb5d85434daba5479010785339ec460d3708c7
Rus@home <punyarit07@gmail.com>
2024-03-24 16:24:29 +0700

    change state with highorder function

--- src/Content.js
+++ src/Content.js
@@ -1,33 +1,55 @@
 import { useState } from 'react';
+import { FaTrashAlt } from 'react-icons/fa';
 
 const Content = () => {
-  const [name, setName] = useState('Rus'); // state name: getter, setName: setter
-  const [count, setCount] = useState(0);
+  const [items, setItems] = useState([
+    {
+      id: 1,
+      checked: false,
+      item: "One half pound bag of Cocoa Covered Almonds Unsalted"
+    },
+    {
+      id: 2,
+      checked: false,
+      item: "Item 2"
+    },
+    {
+      id: 3,
+      checked: false,
+      item: "Item 3"
+    }
+  ]); 
 
-  const handleNameChange = () => {
-    const names = ['Bob', 'Rus', 'Dave'];
-    const int = Math.floor(Math.random() * 3);
-    setName(names[int]);
-  }
-
-  const handleClick = () => {
-    setCount(count + 1);
-    console.log(count);
-  };
-
-  const handleClick2 = () => {
-    console.log(`${count}`);
+  const handleCheck = (id) => {
+    const listItems = items.map(
+      (item) => item.id === id ? 
+        { 
+          ...item,
+          checked: !item.checked
+        } :
+        item
+      );
+    setItems(listItems);
   };
 
   return (
-    
     <main>
-      <p onDoubleClick={handleClick}>
-        Hello {name}!
-      </p>
-      <button onClick={handleNameChange}>Chnage Name</button>
-      <button onClick={handleClick}>Click it</button>
-      <button onClick={handleClick2}>Click it</button>
+      <ul>
+        {items.map((item) => (
+          <li className='item' key={item.id}>
+            <input 
+              type="checkbox"
+              onChange={() => handleCheck(item.id)}
+              checked={item.checked} 
+            />
+            <label>{item.item}</label>
+            <FaTrashAlt 
+              role="button" 
+              tabIndex="0"
+            />
+          </li>
+        ))}
+      </ul>
     </main>
   )
 }
