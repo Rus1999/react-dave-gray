4df5a5e423894939056ac91a8b6c0abb5c709855
Rus@home <punyarit07@gmail.com>
2024-03-24 19:04:50 +0700

    make sub component

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,13 @@
-const Header = () => {
+const Header = ({ title }) => { // destructuring props
   return (
     <header>
-      <h1>Groceries List</h1>
+      <h1>{title}</h1>
     </header>
   )
+}
+
+Header.defaultProps = {
+  title: "Default Title" // overwrite by parent props in App.js
 }
 
 export default Header
